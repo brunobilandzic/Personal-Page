4ecb512c295891acef9851a49fea24022ddb91ef
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2023-03-13 15:03:05 +0100

    edit home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import styles from "../styles/utils.module.css";
 export default function Home() {
   return (
     <>
@@ -7,10 +7,50 @@ export default function Home() {
         <title>Bruno Bilandzic</title>
       </Head>
       <section>
-        <h1 className="">Hello, I am Bruno.</h1>
+        <h2 className="">Hello, I am Bruno.</h2>
         <p className="">
-          This is my personal page.<br/> I am glad you are wanting to learn more about me.
+          This is my personal page.
+          <br />
+          Here you can read my blog posts, see my education and write me a{" "}
+          <a
+            href="https://www.brunobilandzic.from.hr/messages/new"
+            className={styles.customLink}
+          >
+            message
+          </a>
+          .
         </p>
+        <h4>Contact</h4>
+        <a
+          className={styles.customLink}
+          href="mailto:bruno.bilandzic@outlook.com"
+        >
+          Mail
+        </a>
+        <br />
+        <a
+          className={styles.customLink}
+          target="_blank"
+          href="https://github.com/brunobilandzic"
+        >
+          Github
+        </a>
+        <br />
+        <a
+          className={styles.customLink}
+          target="_blank"
+          href="https://www.facebook.com/bruno.bilandzic.14/"
+        >
+          Facebook
+        </a>
+        <br />
+        <a
+          className={styles.customLink}
+          target="_blank"
+          href="https://www.instagram.com/bruno.bilandzic/"
+        >
+          Instagram
+        </a>
       </section>
     </>
   );
