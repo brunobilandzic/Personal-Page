6187dedbeba4bd6296a98ab2f2d437b5e7a1523b
Bruno Bilandzic <bruno.bilandzic2@gmail.com>
2024-09-21 17:47:48 +0200

    meta

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from "next/head";
+import Image from "next/image";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Bruno Bilandzic</title>
+        <meta name="description" content="Bruno Bilandzic's personal page containing email and github." />
+        <link rel="icon" href="/zgrada_selfi.ico" />
       </Head>
       <div className="main-container">
+        <Image src="/zgrada_selfi.ico" alt="Zgrada Selfi" width={200} height={200} />
         <section>
           <h2>
             <a href="mailto:bruno.bilandzic@outlook.com">Mail</a>
@@ -20,6 +24,6 @@ export default function Home() {
           <br />
         </section>
       </div>
-    </>
+      </>
   );
 }
